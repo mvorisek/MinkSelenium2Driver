01b948eb4d554531b3d507b1ab9c90e900b7b66c
Michael Voříšek <mvorisek@mvorisek.cz>
2023-05-03 14:00:13 +0200

    getText() should return unchanged HTMLElement::innerText

--- src/Selenium2Driver.php
+++ src/Selenium2Driver.php
@@ -541,11 +541,7 @@ class Selenium2Driver extends CoreDriver
      */
     public function getText($xpath)
     {
-        $node = $this->findElement($xpath);
-        $text = $node->text();
-        $text = (string) str_replace(array("\r", "\r\n", "\n"), ' ', $text);
-
-        return $text;
+        return $this->executeJsOnXpath($xpath, 'return {{ELEMENT}}.innerText;');
     }
 
     /**
